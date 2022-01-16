8b4b89a6f7fd27f8762b964f40afe9205633b839
thama23 <94192471+thama23@users.noreply.github.com>
2022-01-16 16:32:59 +0200

    initial

---
+++ test_attention.py
@@ -0,0 +1,20 @@
+import pytest
+import torch
+
+from ludwig.modules.attention_modules import FeedForwardAttentionReducer
+
+
+@pytest.mark.parametrize("input_hidden_size", [128, 256, 512])
+@pytest.mark.parametrize("input_seq_size", [10, 20])
+@pytest.mark.parametrize("input_batch_size", [16, 32])
+def test_feed_forward_attention_reducer(input_batch_size, input_seq_size, input_hidden_size):
+    # Generate synthetic data
+    current_inputs = torch.normal(0, 1, size=[input_batch_size, input_seq_size, input_hidden_size], dtype=torch.float32)
+
+    # instantiate feed forward attention reducer
+    feed_forward_attention_reducer = FeedForwardAttentionReducer(input_hidden_size)
+
+    result = feed_forward_attention_reducer(current_inputs)
+
+    # ensure returned tensor is the correct shape
+    assert list(result.shape) == [input_batch_size, input_hidden_size]
